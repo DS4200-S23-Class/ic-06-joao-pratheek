7aa77705f13356c2a8012be8aea3d99831b2f45e
JoaoPedro-dot <castro.joa@northeastern.edu>
2023-02-13 15:21:27 -0500

    Final changes

---
+++ main.js
@@ -0,0 +1,15 @@
+// JS File for class demo part 2
+// Joao Pedro
+// Last modified: 2.13.23
+
+
+// Event handler for click
+function changeLine() {
+
+
+	let line = document.getElementById("frame")
+}
+
+document.getElementById("subButton")
+	.addEventListener("click", changeLine);
+
